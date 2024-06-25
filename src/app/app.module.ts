6fcdeddb09489311b655bbb4e416115df8d34904
ginomiguel91 <ginomiguel91@gmail.com>
2024-06-25 12:19:19 -0400

    Add new icons,dependencies for google auth,update footer,animations in  catalogue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { SharedModule } from './shared/shared.module';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { environment } from 'src/environments/environment.development';
 
 /* configurar el cargador para admmitir varios idiomas */
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
-
+const firebaseConfig = {
+  apiKey: "AIzaSyD6AIFZWIGUTUkxEzuHHFEF5QUM7pt7GtQ",
+  authDomain: "nails-13c9a.firebaseapp.com",
+  projectId: "nails-13c9a",
+  storageBucket: "nails-13c9a.appspot.com",
+  messagingSenderId: "742962383987",
+  appId: "1:742962383987:web:7ad1fe3c901cdda4146e06",
+  measurementId: "G-1ZQ1MJMMQ8"
+}
 @NgModule({
   declarations: [AppComponent],
   imports: [
@@ -21,6 +32,8 @@ export function HttpLoaderFactory(http: HttpClient) {
     HttpClientModule,
     FontAwesomeModule,
     SharedModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule,
 
     /* agregar esto */
     TranslateModule.forRoot({
